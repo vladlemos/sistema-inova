aa8074c1950dd423515938f2480acc298fdee66d
EduardoChuman <edu.chuman@hotmail.com>
2019-04-17 23:04:12 -0300

    resolucao do consumo de json multinivel e organizacao do arquivo antigo de cadastro multiplos com um post

--- public/js/consumoJsonMultinivel.js
+++ public/js/consumoJsonMultinivel.js
@@ -4,47 +4,69 @@ $( document ).ready(function() {
 });
 
 function consumoJsonMultinivel(){
-    // $.ajax({
-    //     type: 'GET',
-    //     url: '/api/bndes/v2/siaf_amortizacoes/3000',
-    //     context: this,
-    //     data: dados,
-    //     success: function(dados){
-    //         console.log(dados);
-    //     },
-    //     error: function(error){
-    //         console.log(error);
-    //     }
-    // });
-    $.get( "https://inova.ceopc.des.caixa/sistemas/public/api/bndes/v2/siaf_amortizacoes/3000", function( dados ) {
-        var jsonObjeto = jQuery.parseJSON(dados);
-        // // var jsonSaldo = jQuery.parseJSON(consultaSaldo);
-        // for(i = 0; i < jsonObjeto.consultaSaldo.length; i++){
-            
-        //         dataConsultaSaldo = jsonObjeto.consultaSaldo[i].dataConsultaSaldo.date,
-                 
-            
-        //     console.log(dataConsultaSaldo);
-        // }
-        // console.log(jsonObjeto);
-        // // console.log(jsonSaldo);
+    $.get( "/dados.json", function( dados ) {
+        for(i = 0; i < dados.consultaSaldo.length; i++){
+            // console.log(json[i]);
+            linha = montaLinhaTabelaSaldo(dados.consultaSaldo[i]);
+            $('#tabelaConsultaSaldo>tbody').append(linha);
+        }              
+        function montaLinhaTabelaSaldo(dadosSaldo)
+        {
+            linha = '<tr>' +
+                        '<td>' + dadosSaldo.dataConsultaSaldo.date.toLocaleString() + '</td>' +
+                        '<td>' + dadosSaldo.statusSaldo + '</td>' +
+                        '<td>' + dadosSaldo.saldoDisponivel + '</td>' +
+                        '<td>' + dadosSaldo.saldoBloqueado + '</td>' +
+                        '<td>' + dadosSaldo.LimiteChequeAzul + '</td>' +
+                        '<td>' + dadosSaldo.LimiteGim + '</td>' +
+                        '<td>' + dadosSaldo.saldoTotal + '</td>' +
+                    '</tr>';
+            return linha;
+        }
 
-        // codigoDemanda = jsonObjeto.codigoDemanda;
-        // nomeCliente = jsonObjeto.nomeCliente;
-        // cnpj = jsonObjeto.cnpj;
-        // status = jsonObjeto.status;
-        // contratoCaixa = jsonObjeto.contratoCaixa;
-        // contratoBndes = jsonObjeto.contratoBndes;
-        // contaDebito = jsonObjeto.contaDebito;
-        // tipoOperacao = jsonObjeto.tipoOperacao;
-        // valorOperacao = jsonObjeto.valorOperacao;
-        // codigoPa = jsonObjeto.codigoPa;
-        // codigoSr = jsonObjeto.codigoSr;
-        // codigoGigad = jsonObjeto.codigoGigad;
-        
-        var objectKeys = Object.keys(jsonObjeto);
-        console.log(‘objectKeys: ‘, objectKeys);
- 
-        
+        for(i = 0; i < dados.historicoContrato.length; i++){
+            // console.log(json[i]);
+            linha = montaLinhaTabelaHistorico(dados.historicoContrato[i]);
+            $('#tabelaHistoricoContrato>tbody').append(linha);
+        }              
+        function montaLinhaTabelaHistorico(dadosHistorico)
+        {
+            linha = '<tr>' +
+                        '<td>' + dadosHistorico.dataHistorico.date.toLocaleString() + '</td>' +
+                        '<td>' + dadosHistorico.statusHistorico + '</td>' +
+                        '<td>' + dadosHistorico.matriculaResponsavel + '</td>' +
+                        '<td>' + dadosHistorico.unidadeResponsavel + '</td>' +
+                        '<td>' + dadosHistorico.observacaoHistorico + '</td>' +
+                    '</tr>';
+            return linha;
+        }
+
+        codigoDemanda = dados.codigoDemanda;
+        nomeCliente = dados.nomeCliente;
+        cnpj = dados.cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+        status = dados.status;
+        contratoCaixa = dados.contratoCaixa;
+        contratoBndes = dados.contratoBndes;
+        contaDebito = dados.contaDebito;
+        tipoOperacao = dados.tipoOperacao;
+        valorOperacao = dados.valorOperacao;
+        codigoPa = dados.codigoPa;
+        codigoSr = dados.codigoSr;
+        codigoGigad = dados.codigoGigad;
+
+        document.getElementById('codigoDemanda').value = codigoDemanda;
+        document.getElementById('nomeCliente').value = nomeCliente;
+        document.getElementById('cnpj').value = cnpj;
+        document.getElementById('status').value = status;
+        document.getElementById('contratoCaixa').value = contratoCaixa;
+        document.getElementById('contratoBndes').value = contratoBndes;
+        document.getElementById('contaDebito').value = contaDebito;
+        document.getElementById('tipoOperacao').value = tipoOperacao;
+        document.getElementById('valorOperacao').value = valorOperacao;
+        document.getElementById('codigoPa').value = codigoPa;
+        document.getElementById('codigoSr').value = codigoSr;
+        document.getElementById('codigoGigad').value = codigoGigad;
+
+        // console.log(dados);
     });
 }
--- resources/views/consumoJsonMultinivel.blade.php
+++ resources/views/consumoJsonMultinivel.blade.php
@@ -1,18 +1,61 @@
-<?php
-
-echo "chegou";
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Consumo Json Multinivel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        NOME: <input class="form-control" type="text" id="nomeCliente" value="" readonly="readonly"/><br/>
+        CNPJ: <input class="form-control" type="text" id="cnpj" value="" readonly="readonly"/><br/>
+        DEMANDA: <input class="form-control" type="text" id="codigoDemanda" value="" readonly="readonly"/><br/>
+        CONTRATO BNDES: <input class="form-control" type="text" id="contratoBndes" value="" readonly="readonly"/><br/>
+        CONTRATO CAIXA: <input class="form-control" type="text" id="contratoCaixa" value="" readonly="readonly"/><br/>
+        CONTA DEBITO: <input class="form-control" type="text" id="contaDebito" value="" readonly="readonly"/><br/>
+        VALOR OPERACAO: <input class="form-control" type="text" id="valorOperacao" value="" readonly="readonly"/><br/>
+        TIPO OPERACAO: <input class="form-control" type="text" id="tipoOperacao" value="" readonly="readonly"/><br/>
+        STATUS: <input class="form-control" type="text" id="status" value="" readonly="readonly"/><br/>
+        CODIGO PA: <input class="form-control" type="text" id="codigoPa" value="" readonly="readonly"/><br/>
+        CODIGO SR: <input class="form-control" type="text" id="codigoSr" value="" readonly="readonly"/><br/>
+        CODIGO GIGAD: <input class="form-control" type="text" id="codigoGigad" value="" readonly="readonly"/><br/>
+
+        <table class="table table-ordered table-hover table-striped" id='tabelaConsultaSaldo'>
+            <thead>
+                <tr>
+                    <th>DATA CONSULTA SALDO</th>
+                    <th>STATUS SALDO</th>
+                    <th>SALDO DISPONIVEL</th>
+                    <th>SALDO BLOQUEADO</th>
+                    <th>LIMITE CHEQUE AZUL</th>
+                    <th>LIMITE GIM</th>
+                    <th>SALDO TOTAL</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table> 
+
+        <table class="table table-ordered table-hover table-striped" id='tabelaHistoricoContrato'>
+            <thead>
+                <tr>
+                    <th>DATA HISTORICO</th>
+                    <th>STATUS HISTORICO</th>
+                    <th>MATRICULA RESPONSAVEL</th>
+                    <th>UNIDADE RESPONSAVEL</th>
+                    <th>OBSERVACAO HISTORICO</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table> 
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/consumoJsonMultinivel.js')}}"></script>
 </body>
