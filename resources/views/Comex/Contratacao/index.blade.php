e5f34a2a36b2741e2a907eff6ad445a52d9eda37
VLADIMIR PEREIRA DE LEMOS <vladimir.lemos@caixa.gov.br>
2019-05-24 15:49:37 -0300

    pagina template ok

--- resources/views/Comex/Contratacao/index.blade.php
+++ resources/views/Comex/Contratacao/index.blade.php
@@ -1,3 +1,20 @@
-<?php
 
-echo "Esteira Contratação";
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Esteira de Contratação</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('####################### OLAH #####################'); </script>
+@stop
