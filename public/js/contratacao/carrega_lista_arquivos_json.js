306bf3fd91c32b836b0e7943a91fc941aa1d29c0
CARLOS ALBERTO DALCIN DAVID <carlos.a.david@caixa.gov.br>
2019-06-05 15:10:06 -0300

    teste de preview de arquivos tela analise

---
+++ public/js/contratacao/carrega_lista_arquivos_json.js
@@ -0,0 +1,241 @@
+$(document).ready(function() {
+
+    // var cpfCnpj = '10.222.2220001-88';
+    // var protocolo ='546654';
+
+    // var cpfCnpj = $("#cpfCnpj").val();
+    // var protocolo = $("#idDemanda").val();
+
+    var cpfCnpj = $("#cpfCnpj").html();
+    var protocolo = $("#idDemanda").html();
+
+    // var json = {
+    //     "fileInvoice" : "'http://www.ceopc.hom.sp.caixa/esteira-contratacao/upload-teste/10222222000188/546654/invoice_546654.jpg','",
+    //     "captionInvoice" : "'{caption: 'invoice_546654.jpg'},'",
+    //     "fileDi" : "'http://www.ceopc.hom.sp.caixa/esteira-contratacao/upload-teste/10222222000188/546654/di_546654.pdf','",
+    //     "captionDi": "'{caption: 'di_546654.pdf'},'",
+    //     "fileConhecimento": "'http://www.ceopc.hom.sp.caixa/esteira-contratacao/upload-teste/10222222000188/546654/conhecimento_546654.docx','",
+    //     "captionConhecimento": "'{caption: 'conhecimento_546654.docx'},'"
+    // };
+
+    // var invoiceUrl = {
+        
+    // };
+    // var di = {
+        
+    // };
+    // var conhecimento = {
+        
+    // };
+    var urlInvoice ='';
+    var fileInvoice = '';
+    var typeInvoice = '';
+    var urlDi = '';
+    var fileDi = '';
+    var typeDi = '';
+    var urlConhecimento = '';
+    var fileConhecimento = '';
+    var typeConhecimento = '';
+    var urlSr = '';
+    var fileSr = '';
+    var typeSr = '';
+
+
+// $.ajax({
+//     type: 'GET',
+//     url: 'tabela_analise_arquivos.json',
+//     // data: { get_param: 'value' },
+//     dataType: 'JSON',
+//     success: function(data){
+//         // var data = data;
+        
+//         fileInvoice = data[0].fileInvoice;
+//         captionInvoice = data[0].captionInvoice;
+//         fileDi = data[0].fileDi;
+//         captionDi = data[0].captionDi;
+//         fileConhecimento = data[0].fileConhecimento;
+//         captionConhecimento = data[0].captionConhecimento;
+//         console.log(data);
+    // },
+    // });
+
+    $.ajax({
+        type: 'GET',
+        url: '../../js/contratacao/tabela_analise_arquivos2.json',
+        // data: { get_param: 'value' },
+        dataType: 'JSON',
+        success: function(data){
+            // var data = data;
+            
+            urlInvoice = data[0].urlInvoice;
+            fileInvoice = data[0].fileInvoice;
+            typeInvoice = data[0].typeInvoice;
+            urlDi = data[0].urlDi;
+            fileDi = data[0].fileDi;
+            typeDi = data[0].typeDi;
+            urlConhecimento = data[0].urlConhecimento;
+            fileConhecimento = data[0].fileConhecimento;
+            typeConhecimento = data[0].typeConhecimento;
+            urlSr = data[0].urlSr;
+            fileSr = data[0].fileSr;
+            typeSr = data[0].typeSr;       
+            console.log(data);
+    
+
+
+
+// salvaVariaveis();
+
+// $("#input-iconic").fileinput({
+$(":file").fileinput({
+    fileActionSettings: {
+        showRemove: false,
+        showUpload: false,
+        showZoom: true,
+        showDrag: false,
+    },
+    showBrowse: false,
+    theme: 'fa',
+    language: 'pt-BR',
+    uploadUrl: 'http://sp0000sr055/diretoriovirtual$/upload-teste/',
+    minFileCount: 1,
+    maxFileCount: 5,
+    overwriteInitial: false,
+    previewFileIcon: '<i class="fas fa-file"></i>',
+    allowedPreviewExtensions: ["pdf", "doc", "docx", "txt", "zip", "jpg", "png", "jpeg"],
+    allowedFileExtensions: ["jpg", "jpeg", "png","pdf"],
+    msgInvalidFileExtension: "O tipo de arquivo selecionado não é suportado. Favor selecionar um arquivo de imagem ou PDF." ,
+
+    // accept=".pdf,.doc,.docx,.txt,.zip,.jpg,.gif,.png" 
+
+    initialPreview: [
+        
+        urlInvoice, 
+        urlDi, 
+        urlConhecimento,
+        urlSr,
+
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.jpg',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/di_' + protocolo + '.pdf',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/conhecimento_' + protocolo + '.docx',
+
+
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.jpg',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/di_' + protocolo + '.pdf',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/conhecimento_' + protocolo + '.docx',
+            
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.jpeg',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.png',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.pdf',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.rar',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.zip',
+            // 'upload-teste/' + cpfCnpj + '/' + protocolo + '/invoice_' + protocolo + '.7z',
+
+
+            // 'upload-teste/10.222.2220001-88/546654/invoice_546654.jpg',
+
+    ],
+    initialPreviewAsData: true, // defaults markup  
+    initialPreviewFileType: 'image',
+    initialPreviewDownloadUrl: '//sp0000sr055/diretoriovirtual$/upload-teste/' + cpfCnpj + '/' + protocolo + '/{filename}',
+    initialPreviewConfig: [ 
+
+        {caption: fileInvoice},
+        {type: typeDi, caption: fileDi, url: urlDi},
+        {type: typeConhecimento, caption: fileConhecimento, url: urlConhecimento},
+        {type: typeSr, caption: fileSr, url: urlSr},
+
+
+        // {caption: 'invoice_' + protocolo + '.pdf', type: 'pdf'},
+        // {caption: 'invoice_' + protocolo + '.doc', type: 'office'},
+        // {caption: 'invoice_' + protocolo + '.docx', type: 'office'},
+        // {caption: 'invoice_' + protocolo + '.txt', type: 'text'},
+        // {caption: 'invoice_' + protocolo + '.zip'},
+        // {caption: 'invoice_' + protocolo + '.jpg'},
+        // {caption: 'invoice_' + protocolo + '.png'},
+        // {caption: 'invoice_' + protocolo + '.gif'},
+
+        // {caption: 'di_' + protocolo + '.pdf', type: 'pdf'},
+        // {caption: 'di_' + protocolo + '.doc', type: 'office'},
+        // {caption: 'di_' + protocolo + '.docx', type: 'office'},
+        // {caption: 'di_' + protocolo + '.txt', type: 'text'},
+        // {caption: 'di_' + protocolo + '.zip'},
+        // {caption: 'di_' + protocolo + '.jpg'},
+        // {caption: 'di_' + protocolo + '.png'},
+        // {caption: 'di_' + protocolo + '.gif'},
+
+        // {caption: 'conhecimento_' + protocolo + '.doc', type: 'office'},
+        // {caption: 'conhecimento_' + protocolo + '.docx', type: 'office'},
+        // {caption: 'conhecimento_' + protocolo + '.pdf', type: 'pdf'},
+        // {caption: 'conhecimento_' + protocolo + '.txt', type: 'text'},
+        // {caption: 'conhecimento_' + protocolo + '.zip'},
+        // {caption: 'conhecimento_' + protocolo + '.jpg'},
+        // {caption: 'conhecimento_' + protocolo + '.png'},
+        // {caption: 'conhecimento_' + protocolo + '.gif'},
+
+
+
+        // {caption: 'invoice_' + protocolo + '.jpg'},
+        // {caption: 'di_' + protocolo + '.pdf', type: 'pdf'},
+        // {caption: 'conhecimento_' + protocolo + '.docx', type: 'office'},
+    ],
+
+    // removeFromPreviewOnError: true,
+    purifyHtml: true,
+    uploadExtraData: {
+        img_key: "1000",
+        img_keywords: "happy, nature"
+    },
+    preferIconicPreview: true, // this will force thumbnails to display icons for following file extensions
+         previewFileIconSettings: { // configure your icon file extensions
+        'doc': '<i class="fas fa-file-word text-primary"></i>',
+        'xls': '<i class="fas fa-file-excel text-success"></i>',
+        'ppt': '<i class="fas fa-file-powerpoint text-danger"></i>',
+        'pdf': '<i class="fas fa-file-pdf text-danger"></i>',
+        'zip': '<i class="fas fa-file-archive text-muted"></i>',
+        'htm': '<i class="fas fa-file-code text-info"></i>',
+        'txt': '<i class="fas fa-file-text text-info"></i>',
+        'mov': '<i class="fas fa-file-video text-warning"></i>',
+        'mp3': '<i class="fas fa-file-audio text-warning"></i>',
+        // note for these file types below no extension determination logic 
+        // has been configured (the keys itself will be used as extensions)
+        'jpg': '<i class="fas fa-file-image text-danger"></i>', 
+        'gif': '<i class="fas fa-file-image text-muted"></i>', 
+        'png': '<i class="fas fa-file-image text-primary"></i>'    
+    },
+    previewFileExtSettings: { // configure the logic for determining icon file extensions
+        'doc': function(ext) {
+            return ext.match(/(doc|docx)$/i);
+        },
+        'xls': function(ext) {
+            return ext.match(/(xls|xlsx)$/i);
+        },
+        'ppt': function(ext) {
+            return ext.match(/(ppt|pptx)$/i);
+        },
+        'zip': function(ext) {
+            return ext.match(/(zip|rar|tar|gzip|gz|7z)$/i);
+        },
+        'htm': function(ext) {
+            return ext.match(/(htm|html)$/i);
+        },
+        'txt': function(ext) {
+            return ext.match(/(txt|ini|csv|java|php|js|css)$/i);
+        },
+        'mov': function(ext) {
+            return ext.match(/(avi|mpg|mkv|mov|mp4|3gp|webm|wmv)$/i);
+        },
+        'mp3': function(ext) {
+            return ext.match(/(mp3|wav)$/i);
+        }
+    }
+// }).on('filesorted', function(e, params) {
+//     console.log('File sorted params', params);
+// }).on('fileuploaded', function(e, params) {
+//     console.log('File uploaded params', params);
+});
+},
+});
+
+})
+
--- resources/views/Comex/Contratacao/analise.blade.php
+++ resources/views/Comex/Contratacao/analise.blade.php
@@ -38,12 +38,13 @@
 
 <br>
 
-    <form method="POST" action="" enctype="multipart/form-data" id="formAnaliseDemanda">
+    <form method="POST" action="" enctype="multipart/form-data" class="form-horizontal" id="formAnaliseDemanda">
 
-        <div class="form-group row">
+        <div class="form-group">
+
             <label class="col-sm-1 control-label">CNPJ:</label>
             <div class="col-sm-2">
-                <p class="form-control mascaracnpj" name="cnpj" id="cnpj">10222222000188</p>
+                <p class="form-control mascaracnpj" name="cnpj" id="cpfCnpj">10222222000188</p>
             </div>
 
             <label class="col-sm-1 control-label">Nome:</label>
@@ -61,182 +62,197 @@
                 <p class="form-control" name="srResponsavel" id="srResponsavel">4040</p>
             </div>
 
-        </div>  <!--/form-group row-->
+        </div>  <!--/form-group-->
 
 
-        <div class="form-group row">
+        <div class="form-group">
 
-                <label class="col-sm-1 control-label">Operação:</label>
-                <div class="col-sm-2">
-                    <p class="form-control" name="tipoOperacao" id="tipoOperacao">Pronto Importação</p>
-                </div>
+            <label class="col-sm-1 control-label">Operação:</label>
+            <div class="col-sm-2">
+                <p class="form-control" name="tipoOperacao" id="tipoOperacao">Pronto Importação</p>
+            </div>
 
-                <label class="col-sm-1 control-label">Moeda:</label>
-                <div class="col-sm-1">
-                    <p class="form-control" name="tipoMoeda" id="tipoMoeda">USD</p>
-                </div>
+            <label class="col-sm-1 control-label">Moeda:</label>
+            <div class="col-sm-1">
+                <p class="form-control" name="tipoMoeda" id="tipoMoeda">USD</p>
+            </div>
 
-                <label class="col-sm-1 control-label">Valor:</label>
-                <div class="col-sm-2">
-                    <p class="form-control" name="valorOperacao" id="valorOperacao">66.666,66</p>
-                </div>
-        
-                <label class="col-sm-1 control-label">Data de Embarque:</label>
-                <div class="col-sm-2">
-                    <p class="form-control" name="dataPrevistaEmbarque" id="dataPrevistaEmbarque">12/12/1212</p>
-                </div>
-        
+            <label class="col-sm-1 control-label">Valor:</label>
+            <div class="col-sm-2">
+                <p class="form-control" name="valorOperacao" id="valorOperacao">66.666,66</p>
+            </div>
+    
+            <label class="col-sm-1 control-label">Data de Embarque:</label>
+            <div class="col-sm-2">
+                <p class="form-control" name="dataPrevistaEmbarque" id="dataPrevistaEmbarque">12/12/1212</p>
+            </div>
+    
+        </div>  <!--/form-group-->
+
+        <div class="form-group">
+
+            <label class="col-sm-1 control-label">Dados do Beneficiário:</label>
+            <div class="col-sm-3">
+                <p class="form-control" name="dadosContaBeneficiario1" id="dadosContaBeneficiario1">Nome do Beneficiário</p>
+            </div>
+            <div class="col-sm-3">
+                <p class="form-control" name="dadosContaBeneficiario2" id="dadosContaBeneficiario2">Banco Beneficiário</p>
+            </div>
+            <div class="col-sm-2">
+                <p class="form-control" name="dadosContaBeneficiario3" id="dadosContaBeneficiario3">IBAN IBAN IBAN 00000</p>
+            </div>
+            <div class="col-sm-2">
+                <p class="form-control" name="dadosContaBeneficiario4" id="dadosContaBeneficiario4">Conta</p>
+            </div>
         </div>  <!--/form-row-->
-
-        <div class="form-row">
-
-            <div class="form-row margin10">
-                <label class="control-label">Dados do Beneficiário:</label>
-            </div>  <!--/form-group row col-->
-            
-                <p class="form-control col" name="dadosContaBeneficiario1" id="dadosContaBeneficiario1">Nome do Beneficiário</p>
-                <p class="form-control col" name="dadosContaBeneficiario2" id="dadosContaBeneficiario2">Banco Beneficiário</p>
-                <p class="form-control col" name="dadosContaBeneficiario3" id="dadosContaBeneficiario3">IBAN IBAN IBAN IBAN 00000</p>
-
-        </div>  <!--/form-row-->
-
-
 
     <hr>
 
+        <div class="form-group">
 
-        <div class="form-row">
-
-            <div class="form-row margin10">
-                <label class="control-label">Data de Liquidação:</label>
-                <input name="dataLiquidacao" type="date" placeholder="DD/MM/AAAA">
+            <label class="col-sm-1 control-label">Data de Liquidação:</label>
+            <div class="col-sm-2">
+                <input class="form-control" name="dataLiquidacao" type="date" placeholder="DD/MM/AAAA">
             </div>
 
-            <div class="form-row margin10">
-                <label class="control-label">Número do Boleto:</label>
-                <input name="numeroBoleto" type="number">
+            <label class="col-sm-1 control-label">Número do Boleto:</label>
+            <div class="col-sm-1">
+                <input class="form-control" name="numeroBoleto" type="number">
             </div>
 
-            <div class="form-row margin10">
-                <label class="control-label">Status:</label>
-                <div>
+            <label class="col-sm-1 control-label">Status:</label>
+            <div class="col-sm-3">
                     <select name="status" class="form-control" id="status">
+                        <option value="0">Selecione</option>
                         <option value="1">Inconforme</option>
                         <option value="2">Conforme</option>
                         <option value="3">Conta OK</option>
                         <option value="4">Conferido</option>
                         <option value="5">Cancelar</option>
                     </select>
-                </div>
             </div>
 
-        </div>  <!--/form-row-->
-
-    <br>
+        </div>  <!--/form-group-->
 
     <hr>
 
-    <label class="control-label margin10">Checklist:</label>
+    <div class="page-bar">
+        <h3>Check-list</h3>
+    </div>
 
-    <div class="form-row">
+    <div class="row">
 
-        <div class="form-group margin10">
-            <label class="control-label margin10">Invoice:</label> 
-    <br>
-            <label class="control-label margin10">Conhecimento de Embarque:</label>
-    <br>
-            <label class="control-label margin10">DI / DU-E:</label>
-    <br>
-            <label class="control-label margin10">Dados Bancários:</label>
-    <br>
-            <label class="control-label margin10">Autorização SR:</label>
-        </div>
+    <div class="col-md-6">
 
-        <div class="form-group margin10">
-
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseInvoice" value="conforme">
-                <label class="form-check-label">Conforme</label>
+        <div class="form-group">
+            <label class="col-sm-3 control-label">Invoice:</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="conforme">Conforme
+                </label>
             </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseInvoice" value="inconforme">
-                <label class="form-check-label">Inconforme</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="inconforme">Inconforme
+                </label>
             </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseInvoice" value="naoSeAplica">
-                <label class="form-check-label">N/A</label>
-            </div>
-    <br>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseEmbarque" value="conforme">
-                <label class="form-check-label">Conforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseEmbarque" value="inconforme">
-                <label class="form-check-label">Inconforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseEmbarque" value="naoSeAplica">
-                <label class="form-check-label">N/A</label>
-            </div>
-    <br>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDeclaracao" value="conforme">
-                <label class="form-check-label">Conforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDeclaracao" value="inconforme">
-                <label class="form-check-label">Inconforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDeclaracao" value="naoSeAplica">
-                <label class="form-check-label">N/A</label>
-            </div>
-    <br>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDadosBancarios" value="conforme">
-                <label class="form-check-label">Conforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDadosBancarios" value="inconforme">
-                <label class="form-check-label">Inconforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseDadosBancarios" value="naoSeAplica">
-                <label class="form-check-label">N/A</label>
-            </div>
-    <br>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseAutorizaSr" value="conforme">
-                <label class="form-check-label">Conforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseAutorizaSr" value="inconforme">
-                <label class="form-check-label">Inconforme</label>
-            </div>
-            <div class="form-check form-check-inline padding7">
-                <input class="form-check-input" type="radio" name="analiseAutorizaSr" value="naoSeAplica">
-                <label class="form-check-label">N/A</label>
-            </div>
-
-        </div>
-
-    </div>  <!--/form-row-->
-
-<br>
-
-    <div class="form-row">
-
-        <div class="form-row margin10">
-            <label class="control-label">Observações:</label>
-            <div class="">
-            <textarea class="form-control" rows="5" cols="120" name="analiseCeopc" id="observacoes"></textarea>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="naoSeAplica">N/A
+                </label>
             </div>
         </div>
 
-    </div>  <!--/form-row-->
+        <div class="form-group">
+            <label class="col-sm-3 control-label">Conhecimento:</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="conforme">Conforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="inconforme">Inconforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="naoSeAplica">N/A
+                </label>
+            </div>
+        </div>
 
-    <hr>
+        <div class="form-group">
+            <label class="col-sm-3 control-label">DI / DU-E:</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="conforme">Conforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="inconforme">Inconforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="naoSeAplica">N/A
+                </label>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-3 control-label">Dados Bancários:</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="conforme">Conforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="inconforme">Inconforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="naoSeAplica">N/A
+                </label>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-3 control-label">Autorização SR:</label>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="conforme">Conforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="inconforme">Inconforme
+                </label>
+            </div>
+            <div class="radio col-sm-2">
+                <label>
+                    <input type="radio" name="analiseInvoice" value="naoSeAplica">N/A
+                </label>
+            </div>
+        </div>
+
+    </div>  <!--/col-md-6-->
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Observações:</label>
+            <div class="col-sm-10">
+                <textarea class="form-control" rows="10" name="analiseCeopc" id="observacoes"></textarea>
+            </div>
+        </div>
+    </div>  <!--/col-md-6-->
+
+    </div> <!--/row-->
+
+<hr>
 
     <div class="margin10">
 
@@ -246,11 +262,11 @@
             
     </div><!--/margin10-->
 
-<br>
+<hr>
 
         <div class="form-group row">
-            <div class="col-sm-2">
-            <button type="submit" id="submitBtn" class="btn btn-secondary">Gravar</button>
+            <div class="col-sm-1">
+            <button type="submit" id="submitBtn" class="btn btn-primary">Gravar</button>
             </div>
         </div>
 
@@ -269,9 +285,9 @@
 
 
 @section('css')
-    <link href="{{ asset('css/contratacao/cadastro.css') }}" rel="stylesheet">
-    <link href="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/css/fileinput.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/css/fileinput.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/themes/explorer/theme.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/contratacao/cadastro.css') }}" rel="stylesheet">
      
 
 
@@ -280,6 +296,7 @@
 @section('js')
     <script src="{{ asset('js/plugins/jquery/jquery-1.12.1.min.js') }}"></script>
     <script src="{{ asset('js/plugins/jquery/jquery-ui.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="{{ asset('js/plugins/numeral/numeral.min.js') }}"></script>
 
 
@@ -290,15 +307,11 @@
     <script src="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/themes/fa/theme.js') }}"></script>
     <script src="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/themes/fas/theme.js') }}"></script>
     <script src="{{ asset('js/plugins/kartik-v-bootstrap-fileinput-226d7e0/themes/explorer/theme.js') }}"></script>
+    <script src="{{ asset('js/contratacao/carrega_lista_arquivos_json.js') }}"></script>
 
 
-    <script src="{{ asset('js/plugins/masks/jquery.mask.min.js') }}"></script>
-    <script src="{{ asset('js/contratacao/funcoes_cadastro.js') }}"></script>
-    <script src="{{ asset('js/contratacao/post_cadastro.js') }}"></script>
 
 
-    <!-- <script src="carrega_json_matricula_hidden.js"></script> -->
-    <!-- <script src="assets/js/shared/site.js"></script> -->
 
 
 
