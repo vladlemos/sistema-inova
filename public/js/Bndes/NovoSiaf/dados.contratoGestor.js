f2bd2448a3138b94a2a7bc0a9648a00e7e375094
EduardoChuman <edu.chuman@gmail.com>
2019-06-13 17:33:35 -0300

    ajuste de detalhes na regra do módulo Siaf Amortizacao/Liquidacao

--- public/js/Bndes/NovoSiaf/dados.contratoGestor.js
+++ public/js/Bndes/NovoSiaf/dados.contratoGestor.js
@@ -17,7 +17,7 @@ function carregarTabelaGestor()
 
         $.each(json, function (key, value){
           
-            linha = atualizaTabela(value);
+            linha = atualizaTabelaGestor(value);
             $('#tabelaGestor>tbody').append(linha);
 
         }
@@ -30,4 +30,27 @@ function carregarTabelaGestor()
            
     });
     
+}
+
+//atualiza a tabela para visualizacao dos contratos
+function atualizaTabelaGestor(json)
+{ 
+    bDestroy : true,  
+    linha = '<tr>' +         
+                '<td>' + json.codigoDemanda        + '</td>' +
+                '<td>' + json.nomeCliente      + '</td>' +
+                '<td>' + json.contratoCaixa    + '</td>' +
+                '<td>' + json.contratoBndes    + '</td>' +
+                '<td>' + json.dataLote    + '</td>' +
+                '<td>' + json.valorOperacao.replace(".", ",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.") + '</td>' +
+                '<td>' + json.tipoOperacao.replace("A","AMORTIZAÇÃO").replace("L","LIQUIDAÇÃO")   + '</td>' +
+                '<td>' + json.status.replace("GEPOD RESIDUO SIFBN","RESIDUO SIFBN")		        + '</td>' +
+                '<td>'	+				
+                    '<button class="btn btn-info btn-xs tip visualiza fa fa-binoculars center-block" id="botaoCadastrar" onclick ="visualizaDemanda(\'' + json.codigoDemanda + '\')" ></button> ' + 
+                '</td>' +
+                '<td>'	+				
+                    '<button class="btn btn-warning btn-xs tip edita fa fa-edit center-block" id="botaoEditar" onclick ="editarContrato(\'' + json.codigoDemanda	+ '\')" ></button> ' + 
+                '</td>' +
+            '</tr>';
+    return linha;
 }
